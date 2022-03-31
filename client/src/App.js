446e3d9d774c9a7bc1eccbe5772888cce1408d09
NukaGrizz <nukagrizz@gmail.com>
2022-03-31 01:09:32 -0400

    hack in base react structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,57 @@
-import logo from './cola-bottle.png';
-import './App.css';
+//import logo from './cola-bottle.png';
+//import './App.css';
+
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { ApolloProvider } from '@apollo/react-hooks';
+import ApolloClient from 'apollo-boost';
+
+import Home from "./pages/Home";
+import NoMatch from "./pages/NoMatch";
+import { StoreProvider } from "./utils/GlobalState";
+
+const client = new ApolloClient({
+  //request: (operation) => {
+  //  const token = localStorage.getItem('id_token')
+  //  operation.setContext({
+  //    headers: {
+  //      authorization: token ? `Bearer ${token}` : ''
+  //    }
+  //  })
+  //},
+  uri: '/graphql',
+})
 
 function App() {
   return (
-    
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <Router>
+        <div>
+          <StoreProvider>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route component={NoMatch} />
+            </Switch>
+          </StoreProvider>
+        </div>
+      </Router>
+    </ApolloProvider>
+    //<div className="App">
+     // <header className="App-header">
+      //  <img src={logo} className="App-logo" alt="logo" />
+      //  <p>
+      //    Edit<code>src/App.js</code> and save to reload.
+       // </p>
+       // <a
+        //  className="App-link"
+        //  href="https://reactjs.org"
+        //  target="_blank"
+        //  rel="noopener noreferrer"
+        //>
+       //   Learn React
+       // </a>
+     // </header>
+    //</div>
   );
 }
 
